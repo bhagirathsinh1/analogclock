2b00e6bfd790ab570009fb62a824200eb4efa7de
bhagirathsinh1 <70099455+bhagirathsinh1@users.noreply.github.com>
2022-01-24 18:25:49 +0530

    number showing by paint

--- lib/clock_painter.dart
+++ lib/clock_painter.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -5,6 +7,10 @@ import 'package:flutter/material.dart';
 class ClockPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    const double BASE_SIZE = 320.0;
+    bool showAllNumbers = true;
+    double scaleFactor = size.shortestSide / BASE_SIZE;
+    bool showTicks = true;
     double centerX = size.width / 2;
     double centerY = size.height / 2;
     double radius = min(centerX, centerY);
@@ -12,6 +18,7 @@ class ClockPainter extends CustomPainter {
     double outerRadius = radius - 20;
     double innerRdaius = radius - 30;
     DateTime datetime = DateTime.now();
+    _drawIndicators(canvas, size, scaleFactor, showAllNumbers);
 
     //dashLine for second
     Paint secDashPaint = Paint()
@@ -101,5 +108,33 @@ class ClockPainter extends CustomPainter {
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
+  }
+}
+
+void _drawIndicators(
+    Canvas canvas, Size size, double scaleFactor, bool showAllNumbers) {
+  TextStyle style = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+      fontSize: 16.0 * scaleFactor * 1.0);
+  double p = 10.0;
+  // p += 24.0;
+
+  double r = size.shortestSide / 2;
+  double longHandLength = r - (p * scaleFactor);
+
+  for (var h = 1; h <= 12; h++) {
+    if (!showAllNumbers && h % 3 != 0) continue;
+    double angle = (h * pi / 6) - pi; //+ pi / 2;
+    Offset offset =
+        Offset(longHandLength * cos(angle), longHandLength * sin(angle));
+    TextSpan span = TextSpan(style: style, text: h.toString());
+    TextPainter tp = TextPainter(
+      text: span,
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+    );
+    tp.layout();
+    tp.paint(canvas, size.center(offset - tp.size.center(Offset.zero)));
   }
 }
